4b65b494c02b53754320eeebc3858b64f634fe8c
Jeremiah <jeremiahjacobson10@gmail.com>
2024-01-29 23:41:33 -0500

    made menu dom function to export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { homeDomController } from "./homeDom";
+import { menuDomController } from "./menu";
 import "./style.css";
 console.log("Webpack initialized!");
 
 // homeDomController.Home();
+menuDomController.Menu();
